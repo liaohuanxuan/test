bfeada29a666321b6161de545c53711a88ff14a0
lhx <1085063086@qq.com>
2021-04-10 10:02:51 +0800

    2021410

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -30,41 +31,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
     MapView mMapView;
     AMap aMap;
-    private static final int STROKE_COLOR = Color.argb(180, 3, 145, 255);
-    private static final int FILL_COLOR = Color.argb(10, 0, 0, 180);
-
+    private LatLng latLng;
+    private Button nbutton,mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
+        mMapView =  findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
-        if (aMap == null) {
-            aMap = mMapView.getMap();
-        }
-
+        aMap = mMapView.getMap();
+       // latLng = new LatLng(28.1127,112.98991);
+        //aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,14));
+        //aMap.setMyLocationEnabled(true);//设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false
+        //if (aMap == null) {
+        //    aMap = mMapView.getMap();
+       // }
+        //实现定位蓝点
         MyLocationStyle myLocationStyle;
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocatio=nStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
-
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
 
-        myLocationStyle.showMyLocation(true);
-        myLocationStyle.anchor(1, 1);
-        myLocationStyle.strokeColor(Color.RED);
-        myLocationStyle.strokeColor(Color.GREEN);
-        myLocationStyle.strokeWidth(1);
+
+        nbutton=findViewById(R.id.nbutton);//找到控件
+        mbutton=findViewById(R.id.mbutton);
+        nbutton.setOnClickListener(this);
+        mbutton.setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.nbutton:
+                aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mbutton:
+                aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.button2:
+                clickBankPoint();
+                break;
+
+
+        }
+    }
+    private void clickBankPoint(){
+
+        startActivity(bankpoint.class);
+    }
+    private void startActivity(Class<?> cl) {
+        Intent intent = new Intent(this, cl);
+        startActivity(intent);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
